bf46c3523bf4c5cf0eb44c90590920d6d11b95b2
Justin Chang <justin.p.chang@gmail.com>
2016-06-28 15:32:54 -0400

    Initial Commit

---
+++ Paint/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Paint
+//
+//  Created by Justin on 6/28/16.
+//  Copyright © 2016 Justin Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
